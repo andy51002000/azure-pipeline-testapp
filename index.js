99ff647de3c1646e9d7131d347f1d88d924aa087
andy51002000 <andy51002000@gmail.com>
2019-01-10 04:40:04 -0800

    add get

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 
 var express = require('express');
 var app  =  express();
-app.get('/');
+app.get('/', function(req, res){
+
+
+
+    res.send({ message: 'hello  andy'})
+});
 app.listen(1337);
 
 console.log('hello world');
